1476c21ee81c41a7fb21dabce08ef20ad9edcb9c
inakiPinillos <04inaki.pinillos@gmail.com>
2025-09-20 18:53:24 +0200

    Aplicación con submenu bien

--- triPlan/Pantallas/pantallaPrincipal.js
+++ triPlan/Pantallas/pantallaPrincipal.js
@@ -1,44 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight, Image, TouchableOpacity, Platform } from 'react-native';
-import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { StyleSheet, Text, View, Pressable, Image} from 'react-native';
+import Layout from '../Componentes/layout';
 
 const icon = require('../assets/pamplona.jpeg');
 
-export default function HomeScreen({ navigation }) {
-  const insets = useSafeAreaInsets();  // Usamos el hook para obtener los márgenes seguros
-
+export default function PantallaPrincipal({ navigation }) {
   return (
-    <SafeAreaView style={styles.container}>
-      {/* El contenido principal de la pantalla */}
-      <View style={{ flex: 1 }}>
+    <Layout navigation={navigation}>
+      <View>
         <Text style={styles.title}>Bienvenido a la aplicación triPlan!</Text>
         <Image source={icon} style={styles.image} />
-        <TouchableHighlight
+        <Pressable
           style={styles.button}
-          onPress={() => navigation.navigate('Second')}  // Navegar a la segunda pantalla
+          onPress={() => navigation.navigate('Segunda')}  
         >
           <Text style={styles.buttonText}>Iniciar</Text>
-        </TouchableHighlight>
+        </Pressable>
       </View>
+    </Layout>
 
-      {/* Barra inferior */}
-      <View style={[styles.bottomBar, { bottom: insets.bottom }]}>
-        <TouchableOpacity style={styles.button2} onPress={() => navigation.navigate('Home')}>
-          <Text style={styles.buttonText2}>Opción 1</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.button2} onPress={() => navigation.navigate('Second')}>
-          <Text style={styles.buttonText2}>Opción 2</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.button2} onPress={() => alert('Opción 3')}>
-          <Text style={styles.buttonText2}>Opción 3</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button2} onPress={() => alert('Opción 4')}>
-            <Text style={styles.buttonText2}>Opción 4</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
   );
 }
 
@@ -78,28 +58,5 @@ const styles = StyleSheet.create({
     fontSize: 25,
     fontWeight: 'bold',
     textAlign: 'center',
-  },
-  bottomBar: {
-    flexDirection: 'row', // Alinea los botones en una fila
-    justifyContent: 'space-around', // Distribuye los botones equitativamente
-    alignItems: 'center',
-    width: '100%',
-    height: 60,
-    backgroundColor: '#f1f1f1',
-    position: 'absolute', // Fija la barra en la parte inferior
-    borderTopWidth: 1,
-    borderTopColor: '#ddd',
-    zIndex: 999, // Asegura que la barra esté por encima de otros elementos
-  },
-  button2: {
-    backgroundColor: '#007bff',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-  },
-  buttonText2: {
-    color: '#fff',
-    fontSize: 10,
-    fontWeight: 'bold',
   },
 });
